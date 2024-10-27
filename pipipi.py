2d5ab090ad0c53a90717037d54b48c61f9f90f89
DePoli97 <luigitisci16@ip-10-192-11-155.ec2.internal>
2024-10-27 09:53:50 +0000

    FINAL PUSH maybe..

--- pipipi.py
+++ pipipi.py
@@ -20,7 +20,7 @@ vs = VectorStore(folder_path=folder_dataset_path, csv_path=csv_path, persist_dir
 r = Retriever(VectorStore=vs)
 history = []
 
-with open('/teamspace/studios/this_studio/evaluation.json', 'r', encoding='utf-8') as file:
+with open('/teamspace/studios/this_studio/evaluation_prompts.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
 
 print("Chatbot initialized. Type 'exit' to quit.\n")
@@ -41,8 +41,8 @@ for entry in data:
     # append current input and output in history
     #history.append({"user": query, "llm": output})
 
-    formatted_output = f"Chatbot: {output[0]}\\nLinks: {output[1][:2]}\n"
-    entry['output'] = formatted_output
+    formatted_output = f"{output[0]}\\nLinks: {output[1][:2]}\n"
+    entry['prediction'] = formatted_output
     
     # Display chatbot response
     print(f"Chatbot: {output[0]}\\nLinks: {output[1][:2]}\n")
